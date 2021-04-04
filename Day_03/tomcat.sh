0f05b656b5e0a131d851a168211f1ab853313321
Lukashevich <61234420+LukashevichVitali@users.noreply.github.com>
2021-04-04 22:31:56 +0300

    Delete Day_03 directory

--- Day_03/tomcat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo yum install -y tomcat
-sudo yum install -y tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc
-sudo systemctl enable tomcat
-sudo systemctl start tomcat
